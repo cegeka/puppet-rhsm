f9cb6691692413ad9527d21e7060e98cc78c4309
Fabian Dammekens <fabian.dammekens@cegeka.be>
2017-12-26 15:46:41 +0100

    [RHSM] Only populate repositories if the machine is subscribed

--- lib/facter/rhsm_repos.rb
+++ lib/facter/rhsm_repos.rb
@@ -2,10 +2,16 @@ Facter.add('rhsm_repos') do
   confine osfamily: :RedHat
   setcode do
     repos = Array.[]
-    repo_list = Facter::Core::Execution.exec("subscription-manager repos --list-enabled | awk '/Repo ID:/ {print $3}'")
-    repo_list.each_line do |line|
-      repos.push(line.strip)
+    if Facter::Util::Resolution.exec('subscription-manager status && echo true') == 'true'
+      repo_list = Facter::Core::Execution.exec("subscription-manager repos --list-enabled | awk '/Repo ID:/ {print $3}'")
+      repo_list.each_line do |line|
+        repos.push(line.strip)
+      end
+      Puppet.debug "rhsm_repos.rb RHSM: Machine subscribed"
+      repos
+    else
+      Puppet.debug "rhsm_repos.rb RHSM: Machine not subscribed"
+      repos
     end
-    repos
   end
 end
